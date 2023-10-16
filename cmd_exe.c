4f2d09324313f9f5f1bf8b52339e46e3996552ed
Nyamisachela <nyamisachelagat@gmail.com>
2023-10-16 00:24:46 -0700

    execute command

--- cmd_exe.c
+++ cmd_exe.c
@@ -1,19 +1,40 @@
 #include "Shell.h"
+
 /**
-*execute - runs the commands parsed
-*@argv: null terminated array of commands
-*/
+ * execute - runs the commands parsed
+ * @argv: null-terminated array of commands
+ */
 void execute(char **argv)
 {
+	/* Declare variables */
 	char *cmd = NULL, *processed_cmd = NULL;
+	pid_t child_pid;
+	int status;
 
 	if (argv)
 	{
-		cmd  = argv[0];
+		cmd = argv[0];
 		processed_cmd = get_path(cmd);
-		if (execve(processed_cmd, argv, NULL) == -1)
+		/* Fork a child process */
+		child_pid = fork();
+		if (child_pid == -1)
 		{
-			perror("Error:");
+			perror("Fork failed");
+		}
+		else if (child_pid == 0)
+		{
+			/* This code runs in the child process */
+			if (execve(processed_cmd, argv, NULL) == -1)
+			{
+				perror("Error:");
+				exit(1);
+				/* Terminate the child process if execve fails */
+			}
+		}
+		else
+		{/* This code runs in the parent process */
+		waitpid(child_pid, &status, 0);
+		/* Wait for the child to finish */
 		}
 	}
 }
