1393f045fb53216407d737bf60c621abb48e1ec4
ngari-kev <kevngariwangui@gmail.com>
2023-10-17 10:44:16 +0300

    Hopefully the final

--- cmd_exe.c
+++ cmd_exe.c
@@ -10,18 +10,21 @@ void execute(char **argv)
 	char *cmd = NULL, *processed_cmd = NULL;
 	pid_t child_pid;
 	int status;
-<<<<<<< HEAD
-	char *string_literal;
-=======
->>>>>>> 4f2d09324313f9f5f1bf8b52339e46e3996552ed
 
-	string_literal = "env";
 	if (argv)
 	{
 		cmd = argv[0];
 		processed_cmd = get_path(cmd);
+		/* Check for the "env" command to print the environment */
+		if (strcmp(cmd, "env") == 0)
+		{
+			print_env();
+			return;
+		}
+
 		/* Fork a child process */
 		child_pid = fork();
+
 		if (child_pid == -1)
 		{
 			perror("Fork failed");
@@ -29,26 +32,17 @@ void execute(char **argv)
 		else if (child_pid == 0)
 		{
 			/* This code runs in the child process */
-<<<<<<< HEAD
-			if (strcmp(*argv, string_literal) == 0)
-			{
-				print_env();
-				return;
-			}
-			else if (execve(processed_cmd, argv, NULL) == -1)
-=======
 			if (execve(processed_cmd, argv, NULL) == -1)
->>>>>>> 4f2d09324313f9f5f1bf8b52339e46e3996552ed
 			{
 				perror("Error:");
-				exit(1);
-				/* Terminate the child process if execve fails */
+				exit(1); /* Terminate the child proces*/
 			}
 		}
 		else
-		{/* This code runs in the parent process */
-		waitpid(child_pid, &status, 0);
-		/* Wait for the child to finish */
+		{
+			/* This code runs in the parent process */
+			waitpid(child_pid, &status, 0); 
+			/* Wait for the child to finish */
 		}
 	}
 }
